88ad93f4d1e20b93f8aeb181c027beed892c7fb6
Arsenije Karpic <arsenije.karpic@gmail.com>
2018-02-16 20:55:16 +0100

    Main klasa

---
+++ back/projekat-api/src/main/java/posetime/ProjekatApiApp.java
@@ -0,0 +1,14 @@
+package posetime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjekatApiApp {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(ProjekatApiApp.class, args);
+	}
+
+}
