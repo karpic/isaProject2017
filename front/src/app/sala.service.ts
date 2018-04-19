8cb15cae570bd6d62a4d2c304e3d8de912420a2d
Stefan Lukic <stefan.lukic@yahoo.com>
2018-04-19 13:26:34 +0200

    sale

--- front/src/app/sala.service.ts
+++ front/src/app/sala.service.ts
@@ -4,6 +4,7 @@ import { of } from 'rxjs/observable/of';
 import { catchError, map, tap } from 'rxjs/operators';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Sala } from './models/sala';
+import { NovaSala } from './models/nova-sala';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -21,7 +22,28 @@ export class SalaService {
     return this.http.get<Sala>(this.url+'/'+id);
   }
 
-  
+  insertSala(sala: NovaSala): Observable<NovaSala>{
+    return this.http.post<NovaSala>(this.url, sala, httpOptions).pipe(
+      catchError(this.handleError<NovaSala>('insertSala'))
+    );
+  }
+
+  updateSala(sala: Sala): Observable<Sala> {
+    const id = typeof sala === 'string' ? sala : sala.id;
+    const url = `${this.url}/${id}`;
+    return this.http.put<Sala>(url, sala, httpOptions).pipe(
+      catchError(this.handleError<Sala>('updateSala'))
+    );
+  }
+
+  deleteSala(sala: Sala | string): Observable<Sala> {
+    const id = typeof sala === 'string' ? sala : sala.id;
+    const url = `${this.url}/${id}`;
+
+    return this.http.delete<Sala>(url, httpOptions).pipe(
+      catchError(this.handleError<Sala>('deleteSala'))
+    );
+  }
   constructor(private http: HttpClient) { }
 
 
