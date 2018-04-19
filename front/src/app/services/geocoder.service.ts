ac71de7f7cf0e448999cedd0858dad077f92d6e1
Arsenije Karpic <arsenije.karpic@gmail.com>
2018-04-19 13:37:07 +0200

    gotove mape

--- front/src/app/services/geocoder.service.ts
+++ front/src/app/services/geocoder.service.ts
@@ -5,34 +5,47 @@ import { of } from 'rxjs/observable/of';
 import { catchError, map, tap } from 'rxjs/operators';
 import {} from '@types/googlemaps';
 import { GoogleMapsAPIWrapper, MapsAPILoader } from "@agm/core";
-import {} from '@types/googlemaps';
-
 
 declare var google: any;
 
 @Injectable()
-export class GeocoderService extends GoogleMapsAPIWrapper{
+export class GeocoderService{
 
-  getGeoLocation(address: string): Observable<any> {
-    console.log('Getting address: ', address);
-    let geocoder = new google.maps.Geocoder();
+  getGeocoding(address: string) {
     return Observable.create(observer => {
-        geocoder.geocode({
-            'address': address
-        }, (results, status) => {
-            if (status == google.maps.GeocoderStatus.OK) {
-                observer.next(results[0].geometry.location);
-                observer.complete();
-            } else {
-                console.log('Error: ', results, ' & Status: ', status);
-                observer.error();
-            }
-        });
+        try {
+            //at this point the variable google may be still undefined (google maps scripts still loading)
+            //so load all the scripts, then...
+            this.__loader.load().then(() => {
+                let geocoder = new google.maps.Geocoder();
+                geocoder.geocode({ address }, (results, status) => {
+
+                    if (status === google.maps.GeocoderStatus.OK) {
+                        const place = results[0].geometry.location;
+                        observer.next(place);
+                        observer.complete();
+                    } else {
+                        console.error('Error - ', results, ' & Status - ', status);
+                        if (status === google.maps.GeocoderStatus.ZERO_RESULTS) {
+                            observer.error('Address not found!');
+                        }else {
+                            observer.error(status);
+                        }
+
+                        observer.complete();
+                    }
+                });
+            });
+        } catch (error) {
+            observer.error('error getGeocoding' + error);
+            observer.complete();
+        }
+
     });
   }
 
-  constructor(private __loader: MapsAPILoader, private __zone: NgZone) {
-    super(__loader, __zone);
+
+  constructor(private __loader: MapsAPILoader) {
 
   }
 }
