36c59bec212ec06bc9acc92b91edcb97a2934d5a
Stefan Lukic <stefan.lukic@yahoo.com>
2018-09-16 18:48:46 +0200

    update u servisu za projekcije pozorista

--- front/src/app/projekcija-poz.service.ts
+++ front/src/app/projekcija-poz.service.ts
@@ -3,6 +3,8 @@ import { ProjekcijaPoz } from './models/projekcijaPoz';
 import { Observable } from 'rxjs/Observable';
 import { HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs/observable/of';
+import { catchError, map, tap } from 'rxjs/operators';
 
 
 const httpOptions = {
@@ -24,6 +26,30 @@ export class ProjekcijaPozService {
     return this.http.get<ProjekcijaPoz>(this.url + '/' + id);
   }
 
+  updatePredstava(projekcijaPoz: ProjekcijaPoz): Observable<ProjekcijaPoz> {
+    const id = typeof projekcijaPoz === 'string' ? projekcijaPoz : projekcijaPoz.id;
+    const url = `${this.url}/${id}`;
+    return this.http.put<ProjekcijaPoz>(url, projekcijaPoz, httpOptions).pipe(
+      catchError(this.handleError<ProjekcijaPoz>('updateProjekcijaPoz'))
+    );
+  }
+
   constructor(private http: HttpClient) { }
 
+  
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  
 }
